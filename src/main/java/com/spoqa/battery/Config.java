99b47e38b1df9bc60ef34c0f0b4c5afc0f3801d8
Park Joon-Kyu <segfault87@gmail.com>
2015-02-08 23:51:08 +0900

    turned off debug flag

--- src/main/java/com/spoqa/battery/Config.java
+++ src/main/java/com/spoqa/battery/Config.java
@@ -3,6 +3,6 @@ package com.spoqa.battery;
 public final class Config {
 
     static public boolean DEBUG_DUMP_REQUEST = false;
-    static public boolean DEBUG_DUMP_RESPONSE = true;
+    static public boolean DEBUG_DUMP_RESPONSE = false;
 
 }
